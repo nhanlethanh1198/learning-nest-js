63364e135eef9368892966a89fa158a6969c93da
Le Thanh Nhan <nhanlethanh1198@gmail.com>
2022-04-02 14:11:23 +0700

    [Task Feature] GET POST PATCH DELETE method

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
@@ -9,16 +11,29 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, description: string): Task {
+  public getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  public createTask(createTaskDto: CreateTaskDto): Task {
     const task: Task = {
       id: uuid(),
-      title,
-      description,
+      ...createTaskDto,
       status: TaskStatus.OPEN,
     };
 
     this.tasks.push(task);
 
+    return task;
+  }
+
+  public async deleteTask(id: string): Promise<Task[]> {
+    return this.tasks.filter((task) => task.id !== id);
+  }
+
+  public async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
+    const task = this.getTaskById(id);
+    task.status = status;
     return task;
   }
 }
